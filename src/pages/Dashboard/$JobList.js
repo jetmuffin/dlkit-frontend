55c86521f65f7df2fdf9909f2e07658529e03ceb
Benjamin15122 <15051183877@163.com>
2018-12-09 23:11:58 +0800

    fix text

--- src/pages/Dashboard/$JobList.js
+++ src/pages/Dashboard/$JobList.js
@@ -253,13 +253,13 @@ class JobList extends PureComponent {
           <Card bordered={true}>
             <Row>
               <Col sm={8} xs={24}>
-                <Info title="我的待办" value="8个任务" bordered />
+                <Info title="我的待办(未启用)" value="8个任务" bordered />
               </Col>
               <Col sm={8} xs={24}>
-                <Info title="本周任务平均处理时间" value="32分钟" bordered />
+                <Info title="本周任务平均处理时间(未启用)" value="32分钟" bordered />
               </Col>
               <Col sm={8} xs={24}>
-                <Info title="本周完成任务数" value="24个任务" />
+                <Info title="本周完成任务数(未启用)" value="24个任务" />
               </Col>
             </Row>
           </Card>
